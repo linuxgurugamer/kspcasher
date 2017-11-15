147aa5cf26188008ed6cf45ba1b0bd936d2edbf2
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-11-14 20:36:11 -0500

    removed includes for unneeded dlls

--- KSPCasher/KSPCasherData.cs
+++ KSPCasher/KSPCasherData.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KSPPluginFramework;
 
-using UnityEngine;
-using KSP.UI.Screens;
 
 
 namespace KSPCasher
